f10205e0fbf8c7c089b579ecdb8bf12c92057ce8
Lucas Mari <lucasmari17@gmail.com>
2021-02-10 13:39:35 -0300

    Improve error handling in frontend

--- frontend/src/components/GamesList.js
+++ frontend/src/components/GamesList.js
@@ -12,14 +12,28 @@ const GAMES_QUERY = gql`
 `;
 
 const GamesList = () => {
-  const { loading, error, data } = useQuery(GAMES_QUERY);
+  const { loading, error, data } = useQuery(GAMES_QUERY, {
+    errorPolicy: 'all',
+    onError: ({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        console.log(
+          `[GraphQL error]: ${JSON.stringify(graphQLErrors, null, 2)}`
+        );
+
+      if (networkError) {
+        console.log(
+          `[Network error]: ${JSON.stringify(networkError, null, 2)}`
+        );
+      }
+    },
+  });
 
   return (
     <>
       {loading && <p>Loading...</p>}
-      {error && <pre>{JSON.stringify(error, null, 2)}</pre>}
       <div className="content-container">
         <h1>Games</h1>
+        {error && <p>Error :(</p>}
         <div>
           {data && (
             <>
